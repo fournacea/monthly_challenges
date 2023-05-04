797345086a8d19bfc8609a8fea0d831b0b79e9e4
Adam Fournace <fournacea@gmail.com>
2023-05-03 20:38:24 -0400

    V 2.0

--- challenges/urls.py
+++ challenges/urls.py
@@ -3,10 +3,11 @@ from . import views
 
 
 urlpatterns = [
-    path('index', views.index),
-    path('january', views.january),
-    path('february', views.february),
-    path('march', views.march),
-    path('april', views.april),
+    path('index', views.index, name='index'),
+    path('january', views.january, name='janurary'),
+    path('february', views.february, name='february'),
+    path('march', views.march, name='march'),
+    path('april', views.april, name='april'),
+    path('<month>', views.monthly_challenge, name='monthly_challenge')
 
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -17,3 +17,16 @@ def march(request):
 
 def april(request):
     return HttpResponse("<h1 style='text-align: center; color: orange; margin-top:50px;'>Fast for 16 hours each day</>")
+
+def monthly_challenge(request, month):
+    if month == 'may':
+        challenge_text = "<h1 style='text-align: center; color: red; margin-top:50px;'>Walk the dogs every weekday.</>"
+    elif month == 'june':
+        challenge_text = "<h1 style='text-align: center; color: red; margin-top:50px;'>Walk the dogs every weekday.</>"
+    elif month == 'july':
+        challenge_text = "<h1 style='text-align: center; color: green; margin-top:50px;'>Read 4 books.</>"
+    elif month == 'august':
+        challenge_text = "<h1 style='text-align: center; color: orange; margin-top:50px;'>Fast for 16 hours each day.</>"
+    else:
+        challenge_text = "<h1 style='text-align: center; color: red; margin-top:50px;'>Not a valid month."
+    return HttpResponse(challenge_text)
